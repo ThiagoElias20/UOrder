a41a490a24143c258e8a73367f094f36e63d39d8
ThiagoElias20 <contatothiagoelias22@gmail.com>
2024-11-20 23:59:48 -0300

    Implemented Item, Category, Button, Modal and changed items on firestore

---
+++ components/Restaurant/Item.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="item" @click="addCart()">
+        <img :src="ItemDetails.Image" alt="">
+        <p>{{ ItemDetails.Name }}</p>
+        <p>{{ ItemDetails.Description }}</p>
+        <p>R$ {{ ItemDetails.Price }}</p>
+    </div>
+
+    <UOModal :open.sync="modalAddCart" @close="modalAddCart = false">
+        <img src="" alt="">
+        <h3>Gostaria de adicionar o pedido ao carrinho?</h3>
+        <UOButton>Sim</UOButton>
+        <UOButton>Não</UOButton>
+    </UOModal>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            modalAddCart: false,
+        }
+    },
+    props: {
+        ItemDetails: {},
+    },
+    methods: {
+        addCart() {
+            this.modalAddCart = true;
+        }
+    }
+}
+</script>
+
+<style scoped >
+.item {
+    background-color: red;
+}
+.item img {
+    height: 64px;
+    width: 64px;
+    object-fit: cover;
+}
+</style>
---
+++ components/UO/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="modal" v-if="open">
+        <div class="modal__body">
+            <div class="modal__body--content">
+                <slot></slot>
+            </div>
+        </div>
+        <div class="modal__background" @click="closeModal">
+    
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        open: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    methods: {
+        closeModal() {
+            this.$emit("close"); // Emite para o pai deixar o open como false
+        },
+    },
+};
+</script>
+
+
+<style scoped>
+.modal {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+}
+.modal__body {
+    position: fixed;
+    top: 25%;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 10px;
+}
+.modal__body--content {
+    padding: 10px;
+}
+.modal__background {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.295);
+}
+</style>
